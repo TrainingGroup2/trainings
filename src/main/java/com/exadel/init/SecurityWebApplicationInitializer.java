366d25143261e13c1308a66a82eaf201dcf1d780
Victoria <vika95vika@gmail.com>
2015-07-24 11:33:02 +0300

    simple authorization

---
+++ src/main/java/com/exadel/init/SecurityWebApplicationInitializer.java
@@ -0,0 +1,8 @@
+package com.exadel.init;
+
+import org.springframework.security.web.context.*;
+
+public class SecurityWebApplicationInitializer
+        extends AbstractSecurityWebApplicationInitializer {
+
+}
