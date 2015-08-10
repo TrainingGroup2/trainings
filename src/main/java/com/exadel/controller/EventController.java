93feb9310716af99abacc1b766da32ecdd55739e
IPoyda <rock.ivan.poyda@gmail.com>
2015-08-10 20:46:32 +0300

    fixed polling

--- src/main/java/com/exadel/controller/EventController.java
+++ src/main/java/com/exadel/controller/EventController.java
@@ -82,7 +82,10 @@ public class EventController {
             eventDTOs.add(event.toEventDTO());
         }
         result.addAll(eventDTOs);
-        List <Event> updates = events.subList(eventIndex,events.size());
+        List <Event> updates = new ArrayList<>();
+        if(eventIndex<=events.size()) {
+            updates = events.subList(eventIndex, events.size());
+        }
         if (updates.size()!=0) {
             deferredResult.setResult(result);
         }
@@ -102,6 +105,16 @@ public class EventController {
         }
         else {
             userFeedbackEventService.addEvent(eventDTO.toUserFeedbackEvent());
+        }
+        for (Map.Entry<DeferredResult<List<EventDTO>>, Integer> entry : EventController.eventRequests.entrySet()) {
+            List<Event> events = new ArrayList<>();
+            events.addAll(trainingEventService.getUnwatchedEvents());
+            events.addAll(trainingFeedbackEventService.getUnwatchedEvents());
+            List<EventDTO> result = new ArrayList<>();
+            for (Event event: events){
+                result.add(event.toEventDTO());
+            }
+            entry.getKey().setResult(result);
         }
         //TODO: EXCEPTION
     }
